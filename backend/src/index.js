b576aaa245e27da0e74690913db542d86ae0af0e
Wesley Guedes <wesleydasilvaguedes@gmail.com>
2020-03-27 20:49:52 -0300

    Adicionando validação com celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,18 @@ const express = require('express');
 // import cors
 const cors = require('cors');
 
-// importando arquivo de rotas
+// import routes files
 const routes = require('./routes');
+
+// import celebrate to trait response errors
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 /*
 app.get('/', (request, response) => {
